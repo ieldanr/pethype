df9fb698f5d13973e75de11c989fdbf81c54fdc1
Daniel Rios <drios@ooyala.com>
2013-11-10 14:51:53 -0600

    Revert "Adds fonts to assets"
    This reverts commit 1169729c6895082dd06b10b8ec6a8fc4744d1c13.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,6 @@ Pethype::Application.configure do
   # Set to :debug to see everything in the log.
   config.log_level = :info
 
-  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
